d15101afe4bd1756f0f0185b5278fab9dfb2244d
Sergey Kasmy <SleeplessSloth79@gmail.com>
2023-04-01 17:53:22 +0200

    Assign the start range and re-assign the end
    there's still the middle to reassign too

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,21 @@ public:
 	// If !( keyBegin < keyEnd ), this designates an empty interval,
 	// and assign must do nothing.
 	void assign(K const& keyBegin, K const& keyEnd, V const& val) {
+		std::cerr << "Called assign(" << keyBegin << ", " << keyEnd << ", " << val << ")" << std::endl;
 		if (!(keyBegin < keyEnd)) return;
 
-		m_map.insert(keyBegin, val);
-		// TODO
+		auto prev = m_map.lower_bound(keyEnd);
+		if (prev != m_map.begin()) {
+			prev--;
+			std::cerr << "Reassigning" << keyEnd + 1 << " -> " << prev->second << std::endl;
+			m_map.insert_or_assign(keyEnd + 1, prev->second);
+		} else {
+			std::cerr << "lower bound at map.end()" << std::endl;
+		}
+
+
+		std::cerr << "Assigning " << keyBegin << " -> " << val << std::endl;
+		m_map.insert_or_assign(keyBegin, val);
 	}
 
 	// look-up of the value associated with key
@@ -36,10 +47,26 @@ public:
 	}
 };
 
+void print_map(const interval_map<int, char> &map) {
+	for (int i = -5; i < 10; i++) {
+		std::cerr << i << " -> " << map[i] << std::endl;
+	}
+}
+
 // Many solutions we receive are incorrect. Consider using a randomized test
 // to discover the cases that your implementation does not handle correctly.
 // We recommend to implement a test function that tests the functionality of
 // the interval_map, for example using a map of int intervals to char.
 int main() {
+	interval_map<int, char> map('A');
+	print_map(map);
+
+	map.assign(0, 5, 'C');
+	print_map(map);
+
+	map.assign(1, 3, 'B');
+	std::cerr << "Done" << std::endl;
+	print_map(map);
+
 	return 0;
 }
